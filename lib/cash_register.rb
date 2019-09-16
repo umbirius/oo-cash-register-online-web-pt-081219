06387f5b5a6ef71dc9c28fa5fd8a29fa8e14b115
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-16 17:29:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,7 +32,7 @@ class CashRegister
     if @discount > 0 
       @to_take_off = (price*discount)/100
       @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}"
+      return "After the discount, the total comes to $#{total}."
     else 
       return "There is no discount to apply."
     end 
